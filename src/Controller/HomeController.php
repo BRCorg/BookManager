c3fdb6a27512518d74e3ec05bbdbd168ac093dbc
BRCorg <guvenberancan1@gmail.com>
2025-09-29 22:16:30 +0200

    Ajout d'un HomeController pour avoir directement accès aux livres, changement redirection

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function home(): Response
+    {
+        return $this->redirectToRoute('book_index');
+    }
+}
+
